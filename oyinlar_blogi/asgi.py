15fb8d41553a648c9b229fbb1092c8c2fe3b6a05
husannigmatjonov343-collab <husannigmatjonov343@gmail.com>
2026-07-17 16:05:46 +0500

    test

--- oyinlar_blogi/asgi.py
+++ oyinlar_blogi/asgi.py
@@ -3,6 +3,9 @@ ASGI config for oyinlar_blogi project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
+
+
+
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
