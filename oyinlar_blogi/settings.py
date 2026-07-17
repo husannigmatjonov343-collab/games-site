362659f6b153dd57b80a7cf4ae6737230de9147f
husannigmatjonov343-collab <husannigmatjonov343@gmail.com>
2026-07-17 16:23:25 +0500

    fix: updated database port to 6543 for connection pooling

--- oyinlar_blogi/settings.py
+++ oyinlar_blogi/settings.py
@@ -10,6 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import dj_database_url
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres.qtrelabtkackgrzslobt:!j&3Nj2iELfnApt@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres',
+        conn_max_age=0
+    )
+}
+
+
+
 
 import os
 from pathlib import Path
@@ -70,14 +82,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'oyinlar_blogi.wsgi.application'
-
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
